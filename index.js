dadd31f055b047b745aecc3ce264e21a9ae49b1a
Mikhael Mourao <mikhaelmourao@hotmail.com>
2023-11-07 16:20:19 -0300

    modificacao 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express(); //iniciando o express
 app.get("/", function(req,res){
     
     res.send("<h1>Bem vindo ao meu site!</h1>");
-    res.send("<h1>primeira msg!</h1>");
+    res.send("<h1>primeira msg modificada!</h1>");
     
 })
 
